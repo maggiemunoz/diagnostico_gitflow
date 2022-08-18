44244af2aeb0211bdd039a290b0315eb8c0fec57
Maggie Muñoz <62019819+maggiemunoz@users.noreply.github.com>
2022-08-17 20:47:39 -0400

    load json

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 def top_retweeted(json):
     pass
@@ -12,15 +13,29 @@ def top_days(json):
 def top_hashtags(json):
     pass
 
+def load_json(file_name, n):
+    file = open(file_name)
+    lineas = file.readlines()
+    if n < len(lineas):
+        data = json.loads(lineas[n])
+        file.close()
+        return data
+    else:
+        file.close()
+        return -1
 
-def main(name, json):
-    print(name(json))
+
+def main(name, json_file):
+    data = load_json(json_file, 0)
+    print(data)
+    print(type(data))
+    print(name(data))
 
 
 if __name__ == "__main__":
         
     name = sys.argv[1]
-    json = sys.argv[2]
+    json_file = sys.argv[2]
 
-    main(locals()[name], json)
+    main(locals()[name], json_file)
 
