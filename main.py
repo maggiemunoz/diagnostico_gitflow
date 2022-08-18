de038c34e2f3ce4641f4629581cd34d0e223344c
Maggie Muñoz <62019819+maggiemunoz@users.noreply.github.com>
2022-08-17 21:33:28 -0400

    top retweeted

--- main.py
+++ main.py
@@ -4,15 +4,42 @@ import json
 def top_retweeted(json_file):
     i = 0
     tweet_info = load_json(json_file, i)
+
+    top_10_tweets = []
+    top_10_rts = []
+
+    min_rts_top = 10000000000
+    min_top = ""
+
     while tweet_info != -1:
-        print(tweet_info["retweetCount"])
+        rts = tweet_info["retweetCount"]
+        if len(top_10_tweets) == 10:
+            if rts > min_rts_top:
+                top_10_tweets.remove(min_top)
+                top_10_rts.remove(min_rts_top)
+                
+                top_10_tweets.append(tweet_info)
+                top_10_rts.append(rts)
+                
+                min_rts_top = min(top_10_rts)
+                
+                for elem in top_10_tweets:
+                    if elem["retweetCount"] == min_rts_top:
+                        min_top = elem
+                        break
+    
+        else:
+            top_10_tweets.append(tweet_info)
+            top_10_rts.append(tweet_info["retweetCount"])
+            
+            if tweet_info["retweetCount"] < min_rts_top:
+                min_rts_top = tweet_info["retweetCount"]
+                min_top = tweet_info
 
         i += 1
         tweet_info = load_json(json_file, i)
 
-    return "hola"
-
-    pass
+    return top_10_tweets
 
 def top_users(json):
     pass
